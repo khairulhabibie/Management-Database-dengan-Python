c54c774a4b8abe08de0ef09819c276300da3d7af
khairulhabibie <khekhairul@gmail.com>
2022-01-19 14:41:58 +0700

    edit code

--- MySQL Python.py
+++ MySQL Python.py
@@ -1,13 +1,20 @@
 from mysql.connector import connect
 
-# 01 - make a database
+# 01 - membuat koneksi dengan database
+db = connect(
+    host = "localhost",
+    user = "root",
+    password = "sayaBelajar"
+)
+print(db)
+
+# 02 - membuat database
 cursor_db = db.cursor() 
 cursor_db.execute("DROP DATABASE IF EXISTS school")
 cursor_db.execute("CREATE DATABASE IF NOT EXISTS school")
 cursor_db.close()
 db.close()
-
-    # make a new connection to database local
+    # membuat koneksi baru
 db = connect(
   host="localhost",
   user="root",
@@ -15,7 +22,7 @@ db = connect(
   database="school"
 )
 
-# 02 - create a table attribute
+# 03 - membuat atribut table
 cursor_db = db.cursor() 
 cursor_db.execute(
     '''CREATE Table students(
@@ -26,8 +33,8 @@ cursor_db.execute(
     )'''
 )
 
-# 03 - insert data to table
-    # side 1
+# 04 - memasukkan data kedalam table
+    # Cara 1
 query = '''
 INSERT INTO students (nama, id_kelas, tahun_masuk)
 VALUES
@@ -41,40 +48,47 @@ VALUES
 cursor_db.execute(query)
 db.commit()
 
-    # side
+# cara 2
 input_list = [
+
     ('shiroe', 21, 2012),
     ('akatsuki', 21, 2012),
     ('naotsugu', 21, 2012),
     ('minori', 13, 2013),
     ('touya', 13, 2013),
     ('kanami', 31, 2011)
+
 ]
 
 for data in input_list:
 
     query = '''
+
     INSERT INTO students (nama, id_kelas, tahun_masuk)
+
     VALUES (%s, %s, %s)
+
     '''
+
     cursor_db.execute(query, data)
+
 db.commit()
 
 '''
-    # close cusor and database
+    # menutup database
 cursor_db.close()
 db.close()
 '''
 
-# 05 - show data in database
+# 05 - melihat data di dalam table
 query = '''select * from students'''
 cursor_db.execute(query)
 data = cursor_db.fetchall()
 
-    # using loop
+#Untuk menampilkan kita bisa menggunakan for loop,
 for row in data:
     print(row)
 
-    # close database
+# Menutup database
 cursor_db.close()
 db.close()
