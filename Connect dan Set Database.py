bf9b3db4fb2380e5a8fcf2d51e16478aab86ca16
khairulhabibie <khekhairul@gmail.com>
2022-01-20 19:53:12 +0700

    perubahan struktur file

---
+++ Connect dan Set Database.py
@@ -0,0 +1,90 @@
+# library yang digunakan
+from mysql.connector import connect
+
+# 01 - membuat koneksi dengan database
+db = connect(
+    host = "localhost",
+    user = "root",
+    password = "sayaBelajar"
+)
+print(db)
+
+# 02 - membuat database
+cursor_db = db.cursor() 
+cursor_db.execute("DROP DATABASE IF EXISTS school")
+cursor_db.execute("CREATE DATABASE IF NOT EXISTS school")
+cursor_db.close()
+db.close()
+
+# 01 membuat koneksi baru
+db = connect(
+  host="localhost",
+  user="root",
+  password="sayaBelajar",
+  database="school"         
+)
+
+# 02 - membuat atribut table
+cursor_db = db.cursor() 
+cursor_db.execute(
+    '''CREATE Table students(
+        id_siswa int primary key auto_increment,
+        nama VARCHAR(50) not null,
+        id_kelas int not null,
+        tahun_masuk int not null
+    )'''
+)
+
+# 03 - memasukkan data kedalam table
+    # Cara 1
+query = '''
+INSERT INTO students (nama, id_kelas, tahun_masuk)
+VALUES
+    ('shiroe', 21, 2012),
+    ('akatsuki', 21, 2012),
+    ('naotsugu', 21, 2012),
+    ('minori', 13, 2013),
+    ('touya', 13, 2013),
+    ('kanami', 31, 2011)
+'''
+cursor_db.execute(query)
+db.commit()
+
+    # cara 2
+input_list = [
+
+    ('shiroe', 21, 2012),
+    ('akatsuki', 21, 2012),
+    ('naotsugu', 21, 2012),
+    ('minori', 13, 2013),
+    ('touya', 13, 2013),
+    ('kanami', 31, 2011)
+
+]
+
+for data in input_list:
+    query = '''
+    INSERT INTO students (nama, id_kelas, tahun_masuk)
+    VALUES (%s, %s, %s)
+    '''
+    cursor_db.execute(query, data)
+db.commit()
+
+'''
+    # menutup database
+cursor_db.close()
+db.close()
+'''
+
+# 04 - melihat data di database
+query = '''select * from students'''
+cursor_db.execute(query)
+data = cursor_db.fetchall()
+
+#Untuk menampilkan kita bisa menggunakan for loop,
+for row in data:
+    print(row)
+
+# Menutup database
+cursor_db.close()
+db.close()
--- README.md
+++
@@ -1,2 +0,0 @@
-# MySQL-Python
-Python is a database driver. Database drivers are software that gives an application access to connect to a database.
